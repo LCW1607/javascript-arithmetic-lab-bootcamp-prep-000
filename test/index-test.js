0ad3a446f879aeaa1253b5025d126263512c094a
E G <fairwaygrove@gmail.com>
2019-10-08 19:07:51 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -44,8 +44,8 @@ it('dec(n) decrements n and returns the result', function() {
 })
 
 ('preserveDecimal(n)', function() 
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', functionparseFloat.('2.222')
-    (preserveDecimal('2.222')'(2.222) 
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', (functionparseFloat('2.222)'
+     
   
 
     
